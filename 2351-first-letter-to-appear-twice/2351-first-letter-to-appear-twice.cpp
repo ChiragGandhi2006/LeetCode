f95d58806c2def77aacc48cad4f308ad28ef65a7
Chirag Gandhi <gandhichirag2006@gmail.com>
2026-09-16 11:19:41 +0530

    Time: 0 ms (100%), Space: 8.3 MB (69.68%) - LeetHub

---
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    char repeatedCharacter(string s) {
+        unordered_set<char> st;
+
+        for(int i=0;i<s.size();i++){
+            if(st.find(s[i])!=st.end()){
+                return s[i];
+            }
+            st.insert(s[i]);
+        }
+        return ' ';
+    }
+};
